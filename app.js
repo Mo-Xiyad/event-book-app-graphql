1498e34258034aac4259dbf1fe48d067eea56e71
Mo-Xiyad <mohamedziyad@Mohameds-MacBook-Pro.local>
2022-05-01 14:38:39 +0200

    express seerver created and the setup done

---
+++ app.js
@@ -0,0 +1,18 @@
+// const express = require("express");
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+let PORT = 3001 || process.env.PORT;
+
+app.use(express.json());
+app.use(bodyParser.json()); // same as express.json()
+
+app.get("/", (req, res, next) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT} 🚀`);
+});
