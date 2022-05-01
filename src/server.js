865ec2188f4f91d189b6e6752f9291e912c97a2f
Mo-Xiyad <xziyad92@gmail.com>
2022-05-01 18:14:50 +0200

    MogoDB connected and started with the schema models

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import { graphqlHTTP } from "express-graphql"; //middleware function
 import { buildSchema } from "graphql";
+import mongoose from "mongoose";
+import EventModel from "../models/events.js";
 
 const app = express();
 
@@ -9,7 +11,7 @@ let PORT = 3001 || process.env.PORT;
 
 app.use(express.json());
 // app.use(bodyParser.json()); // same as express.json()
-let events = [];
+
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -42,26 +44,54 @@ app.use(
   `),
     rootValue: {
       //points to all the resolver functions
-      events: () => {
-        return events;
+      events: async () => {
+        return await EventModel.find()
+          .then((events) => {
+            console.log(...events);
+            return events;
+            // return events.map((event) => { // leave out all the meta data from the mongoose object collection we get collection
+            //   return { ...event._doc , _id: event.id }; // this ID is translated by mongoose to an string
+            // });
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
       },
-      createEvent: (args) => {
-        const event = {
-          _id: Math.random().toString(),
+      createEvent: async (args) => {
+        const event = await new EventModel({
           title: args.eventInput.title,
           description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: args.eventInput.date,
-        };
-        events.push(event);
-        console.log(event.title);
-        return event;
+          price: +args.eventInput.price, // making a floating number
+          date: new Date(args.eventInput.date),
+        });
+        return await event
+          .save()
+          .then((result) => {
+            console.log(result);
+            return { ...result._doc, _id: result._id.toString() }; // work around for the mongoose object to string conversion
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
       },
     },
     graphiql: true,
   })
 );
 
-app.listen(PORT, async () => {
-  console.log(`Server is running on port ${PORT} 🚀`);
+mongoose.connect(process.env.MONGO_DB_URL);
+
+mongoose.connection.on("connected", () => {
+  console.log("📊 Mongo Connected❗️");
+  app.listen(PORT, () => {
+    console.log(
+      `Server is running on 📍:  http://localhost:${PORT}/graphql 🚀`
+    );
+  });
+});
+
+mongoose.connection.on("error", (error) => {
+  console.log(`Error on connection 💣`, error);
 });
