220d4d98cb26ec43141be655f952799b03a805ac
Mo-Xiyad <mohamedziyad@Mohameds-MacBook-Pro.local>
2022-05-01 16:01:30 +0200

    setup gql starter code

--- src/server.js
+++ src/server.js
@@ -1,16 +1,42 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { graphqlHTTP } from "express-graphql"; //middleware function
+import { buildSchema } from "graphql";
 
 const app = express();
 
 let PORT = 3001 || process.env.PORT;
 
 app.use(express.json());
-app.use(bodyParser.json()); // same as express.json()
+// app.use(bodyParser.json()); // same as express.json()
 
-app.get("/", (req, res, next) => {
-  res.send("Hello World");
-});
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+  type RootQuery {
+    events: [String!]!
+  }
+  type RootMutation {
+    createEvent(name: String!): String
+  }
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+    rootValue: {
+      //points to all the resolver functions
+      events: () => {
+        return ["event1", "event2"];
+      },
+      createEvent: (args) => {
+        return `event created: ${args.name}`;
+      },
+    },
+    graphiql: true,
+  })
+);
 
 app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT} 🚀`);
